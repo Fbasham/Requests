8b4543bc22a9e4a8f22c108cefcbbfd5cd0929e8
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-04-04 10:28:07 -0400

    Update Grafton_ETL.py

--- Grafton_ETL.py
+++ Grafton_ETL.py
@@ -28,32 +28,41 @@ with requests.Session() as s:
 
     x = [i.span.text.replace('\xa0',' ') if i.span else None for i in div]
 
-    # create a list of the indexes of all None types in x
+    #create a list of the indexes of all None types in x:
     idx = [i for i, _ in enumerate(x) if _ is None]
 
 
     with open('test.csv', 'w', newline='') as csvfile:
         
-        headers = ['Inventory', 'Net', 'End Time', 'Driver', 'Carrier', 'Consignee',
-                   'BOL2', 'BOL', 'Supplier', 'Railcar']
+        headers = ['Type','Inventory', 'Net', 'End Time', 'Driver', 'Carrier',
+                   'Consignee', 'BOL', 'GVM BOL', 'Supplier', 'Railcar']
 
         writer = csv.DictWriter(csvfile, fieldnames=headers)
         writer.writeheader()
         
-        '''
-        create a list of lists using slices between the None type indexes
-        if len of row is 7 or 9, it is either a truck lifting or rail offload (i.e. useful rows)
-        insert '' values at given index to normalize row length and to match headers
-        '''
+        rows = []
+        #create a list of lists using slices between the None type indexes:
         for i in range(len(idx)-1):
             row = x[idx[i]+1:idx[i+1]]
             if len(row) in [7,9]:
                 if len(row) is 7:
                     for j in range(3):
                         row.insert(3,'')
+                    row.insert(0,'Rail')
                 else:
                     row.insert(-3,'')
-
+                    row.insert(0,'Truck')
+                            
                 writer.writerow(dict(zip(headers,row)))
+                
+                #used creating a Dataframe with Pandas:
+                rows.append(row)
 
-
+              
+        #alternative to above using Pandas to modify the data and output to csv file:
+        alias = {'1000': 'NGL Supply', '1001': 'Patriot', '2001': 'Ray Energy', '3001': 'NGL Wholesale'}
+        df = pd.DataFrame(rows, columns=headers)
+        df = df[['Type', 'Supplier', 'End Time', 'BOL', 'GVM BOL', 'Railcar', 'Net', 'Inventory']]
+        df = df.sort_values(['Supplier', 'Type'])
+        df['Supplier'] = df['Supplier'].map(alias)
+        df.to_csv('Grafton Inventory Report.csv', index=False)
