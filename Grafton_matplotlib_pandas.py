5ed86d98afa258243b995a61881c2b5c81eff05e
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-04-06 13:55:34 -0400

    Update Grafton_matplotlib_pandas.py

--- Grafton_matplotlib_pandas.py
+++ Grafton_matplotlib_pandas.py
@@ -2,63 +2,47 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import datetime
 
+
 df = pd.read_csv('Grafton Inventory Report.csv', thousands=',')
-##df = df.groupby([df['End Time']]).mean()
 
-##x = df[df['Supplier'] == 'NGL Supply']['End Time']
-##y = df[df['Supplier'] == 'NGL Supply']['Inventory']
-##
-##plt.plot(x,y)
-##plt.show()
-
-
+#Convert to useful units of measure
 df['Inventory'] = df['Inventory'].apply(pd.to_numeric)
 df['End Time'] = pd.to_datetime(df['End Time'], yearfirst=True)
 
-#Average volume offloaded/lifted across grouped time
-df2 = df.groupby([df['End Time'].dt.date]).mean()
-x = df2.index
-y1 = df2['Inventory']
-y2 = df2['Net']
-#plt.plot(x,y2)
-#plt.ylabel('USG')
-#plt.title('Grafton Average Net Volume Lifted/Offloaded')
-#plt.show()
+def supplier_data(supplier):
+    x_mean_date = df[df['Supplier'] == supplier].groupby(df['End Time'].dt.date).mean()
+    x_sum_rail = df[(df['Supplier'] == supplier) & (df['Type'] == 'Rail')].groupby(df['End Time'].dt.date).sum()
+    x_sum_truck = df[(df['Supplier'] == supplier) & (df['Type'] == 'Truck')].groupby(df['End Time'].dt.date).sum().abs()    
+    return [x_mean_date, x_sum_rail, x_sum_truck]
 
 
-#NGL Supply
-x1 = df[df['Supplier'] == 'NGL Supply'].groupby(df['End Time'].dt.date).mean()
-x3 = df[df['Supplier'] == 'NGL Supply'][df['Type'] == 'Rail'].groupby(df['End Time'].dt.date).sum()
-x4 = df[df['Supplier'] == 'NGL Supply'][df['Type'] == 'Truck'].groupby(df['End Time'].dt.date).sum().abs()
-##plt.plot(x3.index, x3['Net'], x4.index, x4['Net'])
-##plt.show()
+#Define supplier variables
+x1,x3,x4 = supplier_data('NGL Supply')
+x2,x5,x6 = supplier_data('Patriot')
 
 
-#Patriot
-x2 = df[df['Supplier'] == 'Patriot'].groupby(df['End Time'].dt.date).mean()
-x5 = df[df['Supplier'] == 'Patriot'][df['Type'] == 'Rail'].groupby(df['End Time'].dt.date).sum()
-x6 = df[df['Supplier'] == 'Patriot'][df['Type'] == 'Truck'].groupby(df['End Time'].dt.date).sum().abs()
-##plt.plot(x5.index, x5['Net'], x6.index, x6['Net'])
-##plt.show()
-
-
-#NGL Supply Inventory at current time
-ngl_inv = df['Inventory'].iloc[df[df['Supplier'] == 'NGL Supply']['End Time'].argmax()]
-pat_inv = df['Inventory'].iloc[df[df['Supplier'] == 'Patriot']['End Time'].argmax()]
-
+#Inventory at current time
 inv_df = pd.concat([x1['Inventory'],x2['Inventory']], axis=1, sort=True)
-inv_df['Inventory'] = inv_df['Inventory'].bfill()
+inv_df['Inventory'] = inv_df['Inventory'].bfill().ffill()
 inv_df.columns = ['NGL', 'Patriot']
 inv_df['Inventory'] = inv_df['NGL'] + inv_df['Patriot']
-xtime = inv_df.index
+inv_xtime = inv_df.index
 
 
+#Begin Plotting
 pd.plotting.register_matplotlib_converters(explicit=True)
-plt.plot(xtime, inv_df['Inventory'], x3.index, x3['Net'], x4.index, x4['Net'], x5.index, x5['Net'], x6.index, x6['Net'],marker='o')
-plt.title(label='Rail/Truck Summary')
-plt.legend(['Inventory', 'NGL Rail', 'NGL Truck', 'PATRIOT Rail', 'PATRIOT Truck'], loc='best')
+plt.plot(inv_xtime, inv_df['Inventory'], x3.index, x3['Net'], x4.index, x4['Net'], x5.index, x5['Net'], x6.index, x6['Net'],
+         marker='o',markersize=3, linewidth=0.75)
+plt.title(label='Grafton Inventory Summary')
+plt.xlabel('Time')
+plt.ylabel('USG')
+plt.legend(['Inventory', 'NGL Rail', 'NGL Truck', 'PATRIOT Rail', 'PATRIOT Truck'], loc='best', fontsize='small')
 
-for i, txt in enumerate(inv_df['Inventory']):
-    plt.annotate(f'{txt//1000}k', (xtime[i], inv_df['Inventory'][i]+2000))
 
-plt.show()
+#Used for generating marker annotations
+marker_arr = [inv_df['Inventory'], x3['Net'], x4['Net'], x5['Net'], x6['Net']]
+for i, frame in enumerate(marker_arr):
+    for j, txt in enumerate(frame):
+        plt.annotate(f'{int(txt/1000)}k', (frame.index[j], frame.values[j]+2000)).set_fontsize(7.5)
+
+plt.show()    
