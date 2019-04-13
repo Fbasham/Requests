3a60dfaf2cd78c0e14984ac57dbfa53c857e2c8e
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-04-13 10:26:53 -0400

    Update meal_finder.py

--- meal_finder.py
+++ meal_finder.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr 12 16:11:43 2019
-
 @author: Fbasham
 """
 
 import requests
 import csv
+from itertools import count
 
 
 def meal_finder(*ingredients):
     
     rows = []
-    for page in range(1,100):
+    page = count(1)
+    while True:   
         
         foods = ','.join(ingredients)
         url = f'http://www.recipepuppy.com/api/?i={foods}'
-
+        
         try:      
-            r = requests.get(url, params={'p': page})
+            r = requests.get(url, params={'p': next(page)})
             for recipe in r.json()['results']:
                 title = recipe['title'].strip()
                 ingr = ', '.join(i.strip() for i in sorted(recipe['ingredients'].split(',')))
                 link = recipe['href']
-                rows.append([title, ingr, link])
-    
+                rows.append([title, ingr, link])    
         except:
+            print('uh no more pages')
             break
-
+     
+    
     with open('Meal Finder.csv', 'w', newline='') as csvfile:
         headers = ['Meal', 'Ingredients', 'Link']
         writer = csv.DictWriter(csvfile, fieldnames=headers)
@@ -36,4 +38,4 @@ def meal_finder(*ingredients):
             writer.writerow(dict(zip(headers, row)))
 
             
-meal_finder('garlic','onion','beef')
+meal_finder('strawberries','milk')
