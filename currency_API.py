3c959a34cd4ae63431f44598e93f09111c8d8f87
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-04-14 11:59:54 -0700

    Add files via upload

--- currency_API.py
+++ currency_API.py
@@ -8,21 +8,41 @@ def get_rate(country1, country2, start, end):
     url = 'https://api.exchangeratesapi.io/history'
     payload = {'base': country1, 'symbols': country2, 'start_at': start, 'end_at': end}
     r = requests.get(url, params=payload)
-
+   
     dates = []
     exchange = []
     for date, rate in sorted(r.json()['rates'].items()):
         dates.append(date)
-        exchange.append(rate[country2])
+        exchange.append(rate)
 
-
-    df = pd.DataFrame({'Date': dates,
-                       'Exchange': exchange}).set_index('Date')
-
+    df = pd.DataFrame(exchange, index=dates)
+ 
     df.plot(title=f'{country1} to {country2}')
     plt.xlabel(xlabel='Date')
     plt.ylabel(ylabel=f'{country1} per {country2}')
     plt.show()
 
 
-get_rate('CAD', 'USD', '1999-01-01', '2019-04-13')
+def get_rates(base, countries, start, end):
+
+    url = 'https://api.exchangeratesapi.io/history'
+    payload = {'base': base, 'symbols': countries, 'start_at': start, 'end_at': end}
+    r = requests.get(url, params=payload)
+
+    dates = []
+    exchange = []
+    for date, rates in sorted(r.json()['rates'].items()):
+        dates.append(date)
+        exchange.append(rates)
+
+    df = pd.DataFrame(exchange, index=dates)
+
+    df.plot(title=f'{base} to {", ".join(countries)}')
+    plt.xlabel(xlabel='Date')
+    plt.ylabel(ylabel=f'{base}')    
+    plt.show()
+
+
+##Sample input:
+get_rate('CAD', 'USD', '2018-01-01', '2019-04-13')
+get_rates('USD', ['CAD', 'GBP', 'EUR'],'1990-04-01', '2019-04-13')
