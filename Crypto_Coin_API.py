6df17d32af3e504df2c05a69422bc1af9e7cbc9e
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-04-17 08:28:58 -0400

    Update Crypto_Coin_API.py

--- Crypto_Coin_API.py
+++ Crypto_Coin_API.py
@@ -1,89 +1,52 @@
-
 # -*- coding: utf-8 -*-
-
 """
-
 Created on Tue Apr 16 16:51:18 2019
-
-
-
 @author: Fbasham
-
 """
-
-
 
 import requests
-
 from datetime import datetime
-
 import pandas as pd
-
 import matplotlib.pyplot as plt
 
 
-
-
-
 def top_coins(top, timeframe):
-
     coin_url = 'https://api.coinranking.com/v1/public/coins'
-
     r = requests.get(coin_url)
 
-
-
     alias = {}
-
     for i in r.json()['data']['coins'][:top]:
-
         alias[i['id']] = i['name']
 
-
-
-    dfs = []
-
+    df = pd.DataFrame()
     for coin_id, coin_name in alias.items():
 
-
-
         url = f'https://api.coinranking.com/v1/public/coin/{coin_id}/history/{timeframe}'
-
         r = requests.get(url)
-
         dates = []
-
         prices = []
 
         for item in r.json()['data']['history']:
-
             time = datetime.fromtimestamp(item['timestamp']/1000)
-
             price = item['price']
-
             dates.append(time)
-
             prices.append(price)
 
-
-
-        frame = pd.DataFrame({coin_name: prices, 'date': dates}).astype({coin_name: float})
-        frame = frame.groupby(frame['date'].dt.date).mean()
-        dfs.append(frame)
+        frame = pd.DataFrame({coin_name: prices}, index=dates).astype(float)
+        frame = frame.resample('D').mean()
+   
+        if df.empty:
+            df = frame
+        else:
+            df = df.join(frame, how='outer')
+  
     
-    
-
-    df = pd.concat(dfs, axis=1, sort=False).interpolate()
-    ##normalized_df=(df-df.mean())/df.std()
+    #normalized_df=(df-df.mean())/df.std()
     normalized_df=(df-df.min())/(df.max()-df.min())
     normalized_df.plot()
 
     plt.show()
 
 
-
-
-
 ##Example function call: 
-
-top_coins(5, '5y')
+top_coins(10, '5y')
