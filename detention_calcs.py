6c2b1e8ac3b235a23bce9f018d8e63aa60e82c3d
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-05-01 12:29:34 -0400

    Add files via upload

--- detention_calcs.py
+++ detention_calcs.py
@@ -1,31 +1,28 @@
 import pandas as pd
 import numpy as np
-from itertools import groupby
+from collections import defaultdict
 
 
 ##Create df for detention terms with demurrage in the description and map mulity tier pricing to a new column
 df_terms = pd.read_csv(r'H:\FBasham\Detention\Detention Terms.csv')
 
 df_terms = df_terms[df_terms['Description'].str.contains('Detention')]
-df_terms['concat'] = df_terms['Deal Line'] + '||' + df_terms['Contract Ref']
+df_terms['key'] = df_terms['Deal Line'] + '||' + df_terms['Contract Ref']
 
-mapper = {}
-for item, group in groupby(zip(df_terms['Deal Line']+'||'+df_terms['Contract Ref'], df_terms['Description.1']), key=lambda x: x[0]):
-    terms = []
-    for thing in list(set(group)):
-        terms.append(thing[1])
-    mapper.update({thing[0]: ''.join(terms)})
+contract_terms = defaultdict(str)
+for key, terms in zip(df_terms['key'], df_terms['Description.1']):
+    contract_terms[key] += terms
 
-df_terms['concat'] = df_terms['concat'].map(mapper)
+df_terms['key'] = df_terms['key'].map(contract_terms)
 
 
 ##Swap Canadian contract contacts with US counterparts 
-marketers = ['Alex Smith', 'Andrew Edwards', 'Doug Begland', 'Dave McCulloch', 'Webster Mundy',
-             'Jason Spillman', 'Brandy Dickie', 'Jelena Rudan', 'David Palmer', 'Shane Wittig',
-             'Bobbi Lowry', 'Chris Skoog', 'Jay Jackson', 'Josh Duval', 'Mike Jones', 'Tom Krupa',
-             'Trevor Budgell']
+marketers = ['Alex Smith', 'Andrew Edwards', 'Doug Begland', 'Dave McCulloch', 'Webster Mundy', 'Jason Spillman', 'Brandy Dickie', 
+             'Jelena Rudan', 'David Palmer', 'Shane Wittig','Bobbi Lowry', 'Chris Skoog', 'Jay Jackson', 'Josh Duval', 
+             'Mike Jones', 'Tom Krupa', 'Trevor Budgell']
 
-df_marketers = pd.read_csv(r'H:\FBasham\Detention\TN Deal-Contract Status.csv', header=15)
+
+df_marketers = pd.read_csv(r'H:\FBasham\Detention\TN Deal-Contract Status.csv')
 df_marketers = df_marketers[df_marketers['Contract Contact'].isin(marketers)]
 
 
@@ -42,4 +39,4 @@ df_marketers = df_marketers[['Contract Ref', 'Contract Contact']]
 ##Join the marketers with the terms
 df = df_terms.merge(df_marketers, on='Contract Ref')
 df = df.drop_duplicates(subset=['Deal Line', 'Origin', 'Destination', 'Contract Ref'])
-df.to_csv('H:\FBasham\Detention\E1 Summary.csv', index=False)
+df.to_csv('H:\FBasham\Detention\E1 Summary Detention Calcs.csv', index=False)
