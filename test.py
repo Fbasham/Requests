54014925c57c87b66063714216229915e1d083d0
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-07-27 20:00:20 -0700

    Update test.py

--- test.py
+++ test.py
@@ -28,3 +28,66 @@ df = df.drop_duplicates(subset=['A', 'B'], keep='last')
 df[['C','D']] = df.groupby('A')[['C','D']].transform(lambda x: x.mean()/len(x))
 print(df)
 
+
+
+#######################################################################################################################################################
+'''unstacking rows with groupby and custom aggregating of columns, useful for invoice upload since the collected raw data is in
+stacked format.  The intent of unstacking it is especially useful for CAD currency codes where we don't want to include tax
+in the upload, or in comparison with merge_asof results for shipments.  Rather, we want to include tax in the final summary 
+for what's being paid per invoice'''
+
+import pandas as pd
+
+
+data = [
+
+    ['111', 'tilx', 'freight', 2000],
+    ['111', 'tilx', 'fsc', 100],
+    ['111', 'tilx', 'tax', 50],
+    ['111', 'cbtx', 'freight', 2000],
+    ['111', 'cbtx', 'fsc', 100],
+    ['111', 'cbtx', 'tax', 50],
+    
+    ['222', 'cctx', 'freight', 3000],
+    ['222', 'cctx', 'fsc', 500],
+    ['222', 'cctx', 'tax', 20],
+    
+    ['333', 'cbtx', 'freight', 2000],
+    ['333', 'cbtx', 'fsc', 100]
+
+    ]
+
+df = pd.DataFrame(data, columns=['inv', 'car', 'desc', 'amt'])
+print(df)
+
+'''     inv   car     desc   amt
+    0   111  tilx  freight  2000
+    1   111  tilx      fsc   100
+    2   111  tilx      tax    50
+    3   111  cbtx  freight  2000
+    4   111  cbtx      fsc   100
+    5   111  cbtx      tax    50
+    6   222  cctx  freight  3000
+    7   222  cctx      fsc   500
+    8   222  cctx      tax    20
+    9   333  cbtx  freight  2000
+    10  333  cbtx      fsc   100'''
+
+
+dfs = df.set_index(['inv', 'car', 'desc']).unstack(level=2, fill_value=0)
+dfs.columns = dfs.columns.droplevel(0)
+dfs = dfs.reset_index()
+dfs = dfs.groupby('inv').agg({'car': list,
+                              'freight': lambda x: x.mean()/len(x),
+                              'fsc': 'mean',
+                              'tax': 'mean'
+                              })
+
+dfs = dfs.explode(column='car').reset_index()
+print(dfs)
+
+'''    inv   car  freight  fsc  tax
+    0  111  cbtx     1000  100   50
+    1  111  tilx     1000  100   50
+    2  222  cctx     3000  500   20
+    3  333  cbtx     2000  100    0'''
