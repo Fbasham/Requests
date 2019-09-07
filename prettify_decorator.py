a08e7c669d599943686da76d1328303f5292e3c5
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-09-06 22:36:40 -0400

    Add files via upload

--- prettify_decorator.py
+++ prettify_decorator.py
@@ -1,30 +1,38 @@
 from functools import wraps
+import asyncio
+import numpy as np
 
 
 def prettify(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f'Function Name: {func.__name__}')
-        if args:
-            print('\nPositional Arugments:')
-            for i, arg in enumerate(args, 1):
-                print(f'    arg {i} = {arg}')
-        if kwargs:
-            print('\nKeyword Arguments:')
-            for k,v in kwargs.items():
-                print(f'    {k} = {v}')
-                
-        print('\nReturn Value: ')
+        print(f'\ncalling {func.__name__}')
+        print(f'args = {args}')
+        print(f"sleeping for: {kwargs.get('delay')} sec")
         return func(*args, **kwargs)
     return wrapper
 
 
+def delay():
+    return np.random.rand(1)[0].round(2)
+
 
 @prettify
-def example_func(*args, bitch=None, **kwargs):
-    return '    this function does nothing'
+async def work(x,y,z,delay=delay):
+    await asyncio.sleep(delay)
+    print(f'finished: x={x:.2f}, y={y:.2f}, z={z:.2f}, delay={delay:.2f}')
+    return z, delay
 
 
-print(example_func(1,'fifty-five', hi='mom'))
+async def main():
+    arr = np.random.rand(3,10).round(2)
+    tasks = (work(x,y,x, delay=delay()) for x,y,z in zip(*arr))
+    results = await asyncio.gather(*tasks)
+    return list(results)
+
+
+if __name__ == '__main__':
+    res = asyncio.run(main())
+    print(res)
 
 
