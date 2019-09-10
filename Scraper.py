45c91850b8f52ea4f058ba7511638bcb546a3e8f
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-09-10 13:41:43 -0400

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -3,41 +3,44 @@ from functools import wraps, partial
 import asyncio
 
 
-class Test():
-
-    def __init__(self, tasks=None):
-        self.tasks = tasks
-        
-    def _async_run(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            result = asyncio.run(func(self, *args, **kwargs))
-            return result
-        return wrapper
-
-    def _tasker(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            task = partial(func, *args, **kwargs)
-            return task
-        return wrapper
-
-    @_tasker
-    async def work(session, **kwargs):
-        return await session.request(**kwargs)
-
-    @_async_run
-    async def scrape(self):
-        session = AsyncHTMLSession()
-        tasks = (task(session=session) for task in self.tasks)
-        return await asyncio.gather(*tasks)
-
-
+class Test:
     
-urls = ('https://www.google.com', 'https://www.reddit.com', 'https://www.python.org',
-        'https://stackoverflow.com/questions', 'https://httpbin.org/')
+    def async_run(func):
+        def wrapper(*args, **kwargs):
+            return asyncio.run(func(*args, **kwargs))
+        return wrapper
 
-tasks = (r.work(method='get', url=url) for url in urls)
+    def tasker(func):
+        def wrapper(*args, **kwargs):
+            return partial(func, *args, **kwargs)
+        return wrapper
+    
+    @tasker
+    async def work(self, session, url, **kwargs):
+        return await session.request(url=url,**kwargs)
+    
+    @async_run
+    async def scrape(self, urls, session=None, **kwargs):  
+        s = session or AsyncHTMLSession()
+        s.loop= asyncio.get_event_loop()
+        tasks = (self.work(url=url, **kwargs)(s) for url in urls)
+        return await asyncio.gather(*tasks)
+    
 
-r = Test(tasks)
-print(r.scrape())
+
+#Example of passing in own session:
+async def get_session():
+    s = AsyncHTMLSession()
+    payload = {'sWebNam': 'username', 'sWebPwd': 'pass', 'Login': 'Login'}
+    r = await s.get('https://company.website.com/lognverf.htm', params=payload)
+    webidx = r.html.search('lWebIdx={}&')[0]
+    return s, webidx
+
+session, webidx = asyncio.run(get_session())
+
+base = 'https://company.website.com/reptcrit.htm?lWebIdx={}&iRepNum=1&iTckLst={}&sRepDat=TMSTRN.MDB'
+urls = (base.format(webidx, ticket) for ticket in range(0,200,25))
+r = Test()
+
+for r in r.scrape(urls, session=session, method='get', headers=None):
+    print(r.content)
