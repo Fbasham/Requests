455ef4e79b0b12be93065ac9e208f77b22ac5065
Fbasham <48450599+Fbasham@users.noreply.github.com>
2019-09-19 18:44:30 -0700

    Add files via upload

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Supplier(models.Model):
     supplier_id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=30)
-    inventory = models.FloatField()
+    inventory = models.FloatField(default=0)
 
     def __str__(self):
         return f'{self.supplier_id}'
@@ -23,6 +23,24 @@ class Receipt(models.Model):
         s = Supplier.objects.get(pk=self.supplier_id.supplier_id)
         s.inventory += self.volume
         s.save()
+
+
+
+class Lifting(models.Model):
+    supplier_id = models.ForeignKey('Supplier', on_delete=models.CASCADE)
+    contract = models.CharField(max_length=20)
+    ticket = models.CharField(max_length=20, primary_key=True)
+    date = models.DateTimeField()
+    volume = models.FloatField()
+
+    def __str__(self):
+        return self.ticket
+
+    def decrement_inventory(self):
+        s = self.supplier_id
+        s.inventory -= self.volume
+        s.save()
+        
 
 
     
